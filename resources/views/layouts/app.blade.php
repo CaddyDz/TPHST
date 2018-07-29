bba36e2ac0d49e8888b3e2a4496286f06f50edfa
Salim Djerbouh <github@caddydz.me>
2018-07-29 11:56:40 +0100

    Move Javascript vendor assets to CDN closed #2
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>SARL TPHST FRERES KHENICHE</title>
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 		{{-- favicon --}}
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"> 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
@@ -27,8 +28,8 @@
     crossorigin="anonymous" />
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <link rel="stylesheet" href="/css/responsive.min.css">
-		{{-- modernizr css--}}
-    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+		{{-- modernizr js --}}
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" integrity="sha256-0rguYS0qgS6L4qVzANq4kjxPLtvnp5nn2nB5G1lWRv4=" crossorigin="anonymous"></script> --}}
 </head>
 
 <body>
@@ -39,7 +40,31 @@
     <div id="preloader"></div>
     @yield('content')
     @include('layouts.footer')
-    @yield('js')
+    {{-- jquery latest version --}}
+    <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+    {{-- bootstrap js --}}
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    {{-- owl.carousel js --}}
+    <script src="https://cdn.jsdelivr.net/gh/CaddyDz/OwlCarousel2@v2.3.5/dist/owl.carousel.min.js"></script>
+    {{-- Counter js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik=" crossorigin="anonymous"></script>
+    {{-- waypoint js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js" integrity="sha256-oP3taRrtdn+FEBHNMYW5KGGSmKIaD72tSAip6ItJCDM=" crossorigin="anonymous"></script>
+    {{-- isotope js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js" integrity="sha256-M4hn22AGHQ12lwC9Kd7yL9Y7zlXpBAAey3pL/i9JErQ=" crossorigin="anonymous"></script>
+    {{-- stellar js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/stellar.js/0.6.2/jquery.stellar.min.js" integrity="sha256-aQ6KGDKk7w8XQNZsQaQnbfeC//XPUIphSp/X/ZEwtV0=" crossorigin="anonymous"></script>
+    {{-- magnific js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js" integrity="sha256-P93G0oq6PBPWTP1IR8Mz/0jHHUpaWL0aBJTKauisG7Q=" crossorigin="anonymous"></script>
+    {{-- venobox js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/venobox/1.7.0/venobox.min.js" integrity="sha256-Q+9tk/RrLpbpICsW8NaZrLZmlXhXcz3WNw2Q6yAgjrw=" crossorigin="anonymous"></script>
+    {{-- meanmenu js --}}
+    <script src="https://cdn.jsdelivr.net/gh/s900cres/meanMenu@v2.0.8/jquery.meanmenu.min.js"></script>
+    {{-- Form validator js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.8.1/validator.min.js" integrity="sha256-ZCYp48kB7gXNRWbK0M6gji4MipHd9j4kTF+ItYoIkRk=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollup/2.4.1/jquery.scrollUp.min.js" integrity="sha256-t2YrqZoTLq/Qt8zIw0BMiuRC2X5+a3O7PODU8RwoyYw=" crossorigin="anonymous"></script>
+    {{-- main js --}}
+    <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,7 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendor/jquery-1.12.4.min.js')
-   .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js')
-   .copy('node_modules/jquery.stellar/jquery.stellar.js', 'public/js/jquery.stellar.min.js')
-   .copy('node_modules/magnific-popup/dist/jquery.magnific-popup.min.js', 'public/js/magnific.min.js')
-   .copy('node_modules/venobox/venobox/venobox.min.js', 'public/js/venobox.min.js')
-   .copy('node_modules/bootstrap-validator/dist/validator.min.js', 'public/js/form-validator.min.js');
+   .sass('resources/assets/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
