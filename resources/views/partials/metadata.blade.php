deffa3436c3aa96867c220b13c3e88d7922ad50e
Salim Djerbouh <github@caddydz.me>
2018-07-30 20:13:07 +0100

    dynamic schedule
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- resources/views/partials/metadata.blade.php
+++ resources/views/partials/metadata.blade.php
@@ -1,15 +1,21 @@
 <ul>
+    @if(setting('site.info_email') != null)
     <li>
         <a href="mailto:{{ setting('site.info_email') }}">
             <i class="fa fa-envelope"></i>{{ setting('site.info_email') }}
         </a>
     </li>
+    @endif
+    @if(setting('site.phone_number') != null)
     <li>
         <a href="tel:{{ setting('site.phone_number') }}">
             <i class="fa fa-phone"></i>{{ setting('site.phone_number') }}
         </a>
     </li>
+    @endif
+    @if(setting('site.schedule') != null)
     <li>
-        <a href="#"><i class="fa fa-clock-o"></i>Dim-Jeu : 8am - 4pm</a>
+        <a><i class="fa fa-clock-o"></i>{{ setting('site.schedule') }}</a>
     </li>
+    @endif
 </ul>
