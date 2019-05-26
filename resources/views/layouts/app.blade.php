09221b0599ba4c54662d6008bd4617fe0dbb34bb
Caddy DZ <caddydz4@gmail.com>
2019-05-26 14:57:47 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: Caddy DZ <caddydz4@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,13 +36,13 @@
 <body>
     <!-- [if lt IE 8]>
 			<p class="browserupgrade">
-			You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+			You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <div id="preloader"></div>
     @yield('content')
     @include('layouts.footer')
     {{-- jquery latest version --}}
-    <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     {{-- bootstrap js --}}
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     {{-- owl.carousel js --}}
