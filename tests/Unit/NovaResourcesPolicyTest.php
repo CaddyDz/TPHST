47a74fc94badb115af0a4bf25e1794590f8666de
Caddy <tbitw31@gmail.com>
2019-06-18 17:54:00 +0100

    nova resource access test
    Signed-off-by: Caddy <tbitw31@gmail.com>

---
+++ tests/Unit/NovaResourcesPolicyTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Laravel\Nova\Nova;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class NovaResourcesPolicyTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * Test that non admin users can't access resources thgrough the admin panel.
+     *
+     * @return void
+     */
+    public function testNovaResourceAccess()
+    {
+        $this->login();
+        Nova::resourcesIn(app_path('Nova'));
+        $resources = Arr::except(Nova::$resources, [0]);
+        foreach ($resources as $resource) {
+            $response = $this->get('nova-api/' . Str::plural(strtolower(class_basename($resource))) . '/cards');
+            $response->assertForbidden();
+        }
+    }
+}
