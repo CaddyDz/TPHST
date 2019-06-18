c20276e4eb43c2f41de10f64af8b35dab47a29ef
Caddy <tbitw31@gmail.com>
2019-06-18 17:52:46 +0100

    login across tests
    Signed-off-by: Caddy <tbitw31@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,19 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use DatabaseMigrations;
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param Illuminate\Contracts\Auth\Authenticatable
+     *
+     * @return TPHST\User authenticated
+     */
+    protected function login($user = null)
+    {
+        $user = $user ?: create('TPHST\User');
+        $this->be($user);
+
+        return $this;
+    }
 }
