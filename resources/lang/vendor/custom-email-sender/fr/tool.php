bc312e40551ea76fd2cf7529f0b7e02c2e12208d
Caddy <tbitw31@gmail.com>
2019-06-20 13:23:40 +0100

    added French localization for custom email sender
    Signed-off-by: Caddy <tbitw31@gmail.com>

---
+++ resources/lang/vendor/custom-email-sender/fr/tool.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tool Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | All of the language translations for the tool that will loaded in to the
+    | Vue components
+    |
+    */
+
+    'tool-name' => 'Expéditeur Email personnalisé',
+
+    // subject
+    'subject-header' => 'Objet',
+    'subject-copy' => 'Entrez l\'objet de votre message',
+    'subject-placeholder' => 'Objet du message',
+
+    // recipients
+    'recipients-header' => 'Destinataires',
+    'recipients-toggle-copy' => 'Souhaitez-vous envoyer ce message à tous les utilisateurs?',
+    'recipients-manual-input-copy' => 'Entrez les adresses électroniques des destinataires auxquelles vous souhaitez envoyer ce message.',
+    'recipients-manual-input-placeholder' => 'Adresses mail',
+
+    // content
+    'content-header' => 'Contenu',
+    'content-copy' => 'Ajoutez le contenu du message que vous souhaitez envoyer.',
+    'content-placeholder' => 'Insérer le texte ici ...',
+
+    // send
+    'send-preview' => 'Envoyer / prévisualiser',
+    'preview-copy' => 'Cliquez sur le bouton ci-dessous pour envoyer et / ou prévisualiser le message.',
+
+    // success
+    'success-header' => 'Les courriels ont été envoyés.',
+    'success-copy' => 'Votre email a bien été envoyé. Si vous souhaitez envoyer un autre message, veuillez cliquer sur le bouton ci-dessous pour recommencer.',
+
+    // buttons
+    'close' => 'Fermer',
+    'send-message' => 'Envoyer le message',
+    'send-message-loading' => 'Envoi. S\'il vous plaît, attendez...',
+    'preview' => 'Aperçu',
+    'preview-loading' => 'Obtenir un aperçu. S\'il vous plaît, attendez...',
+    'start-over' => 'Recommencer',
+
+    // toasts
+    'emails-sent' => 'e-mail(s) ont été envoyés.',
+    'invalid-email' => 'Adresse e-mail invalide.'
+];
