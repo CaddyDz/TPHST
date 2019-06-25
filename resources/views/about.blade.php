f41f19bcac28f20d11c87738004bd136b06a726f
Caddy <tbitw31@gmail.com>
2019-06-25 15:38:28 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: Caddy <tbitw31@gmail.com>

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,23 @@
-@extends('layouts.page') 
+@extends('layouts.page')
+
 @section('content')
+
 @include('layouts.header.bottom', [
     'background' => image('About Background'),
     'headline' => __('About Us'),
-    'title' => __('About Us')
+    'title' => __('About Us'),
 ])
+{{-- Header --}}
 <br>
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="section-headline text-center">
-            <h3>Siége Sociale:</h3>
-            <p>Notre bureau d'étude et administration</p>
+            <h3>@lang('Siége Sociale:')</h3>
+            <p>@lang("Notre bureau d'étude et administration")</p>
         </div>
     </div>
 </div>
+{{-- Office Images --}}
 <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="project-inner project-carousel-2">
         @foreach($images as $image)
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,141 +1,12 @@
-@extends('layouts.app') 
+@extends('layouts.page')
+
 @section('content')
-<div class="topbar-area fix hidden-xs">
-    <div class="container">
-        <div class="row">
-            <div class=" col-md-6 col-sm-6">
-                <div class="topbar-left">
-                    <p>Bienvenu à SARL TPHST</p>
-                </div>
-            </div>
-            <div class="col-md-6 col-sm-6">
-                <div class="topbar-right">
-                    @include('layouts.header.social')
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End top bar -->
-<!-- header-area start -->
-<div class="header-middle-area hidden-xs">
-    <div class="container">
-        <div class="row">
-            <!-- logo start -->
-            <div class="col-md-4 col-sm-3">
-                <div class="logo">
-                    <!-- Brand -->
-                    <a class="navbar-brand page-scroll sticky-logo" href="/">
-                        <img src="/img/logo.png" width="40%" alt="Logo">
-                    </a>
-                </div>
-            </div>
-            <!-- logo end -->
-            <div class="col-md-8 col-sm-9">
-                <div class="header-middle-link">
-                    <div class="header-info">
-                        <div class="header-icon">
-                            <i class="fa fa-phone"></i>
-                        </div>
-                        <div class="header-info-text">
-                            <span class="info-first">{{ setting('phone_number') }}</span>
-                            <span class="info-simple">{{ setting('info_email') }}</span>
-                        </div>
-                    </div>
-                    <div class="header-info">
-                        <div class="header-icon">
-                            <i class="fa fa-map-marker"></i>
-                        </div>
-                        <div class="header-info-text">
-                            <span class="info-first">{{ setting('address') }}</span>
-                            <span class="info-simple">Blida, Algérie</span>
-                        </div>
-                    </div>
-                    <div class="header-info hidden-sm">
-                        <div class="header-icon">
-                            <i class="fa fa-clock-o"></i>
-                        </div>
-                        <div class="header-info-text">
-                            <span class="info-first">Dim-Jeu : 8am - 4pm</span>
-                            <span class="info-simple">Vendredi et Samedi fermé</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End middle bar -->
-<div id="sticker" class="header-area hidden-xs">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-sm-12">
-                <!-- mainmenu start -->
-                <nav class="navbar navbar-default">
-                    <div class="collapse navbar-collapse" id="navbar-example">
-                        <div class="main-menu">
-    @include('layouts.header.navbar_items')
-                        </div>
-                    </div>
-                </nav>
-                <!-- mainmenu end -->
-                <div class="header-right-link">
-                    <!-- search option start -->
-                    {{--
-                    <form action="#">
-                        <div class="search-option">
-                            <input type="text" placeholder="Search...">
-                            <button class="button" type="submit"><i class="fa fa-search"></i></button>
-                        </div>
-                        <a class="main-search" href="#"><i class="fa fa-search"></i></a>
-                    </form> --}}
-                    <!-- search option end -->
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- header-area end -->
-<!-- mobile-menu-area start -->
-<div class="mobile-menu-area hidden-lg hidden-md hidden-sm">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="mobile-menu">
-                    <div class="logo">
-                        <a href="/"><img src="/img/logo.png" alt="Logo" /></a>
-                    </div>
-                    <nav id="dropdown">
-                        @include('layouts.header.navbar_items')
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- mobile-menu-area end -->
-</header>
-<!-- header end -->
-<!-- Start Bottom Header -->
-<div class="page-area" style="background: url('{{ \Storage::url(setting('about.background')) }}')">
-    <div class="breadcumb-overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="breadcrumb text-center">
-                    <div class="section-headline white-head text-center">
-                        <h3>Contactez Nous</h3>
-                    </div>
-                    <ul>
-                        <li class="home-bread">Acceuil</li>
-                        <li>Contactez Nous</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- END Header -->
+
+@include('layouts.header.bottom', [
+    'background' => image('Contact Background'),
+    'headline' => __('Contact Us'),
+    'title' => __('Contact Us'),
+])
 <!-- Start contact Area -->
 <div class="contact-page area-padding">
     <div class="container">
@@ -145,9 +16,11 @@
         @include('contact.map')
     </div>
 </div>
-@stop 
-@section('js')
-<!-- Google Map js -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAdWLY_Y6FL7QGW5vcO3zajUEsrKfQPNzI"></script>
-<script src="/js/mapcode.js"></script>
 @stop
+@env('production')
+    @section('js')
+        <!-- Google Map js -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9kOt-o-2NU7_pcjkGIj4f8cKPw_xinco"></script>
+        <script src="/js/mapcode.js"></script>
+    @stop
+@endenv
