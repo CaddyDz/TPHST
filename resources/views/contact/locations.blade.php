0a0579a61e76df60289ecb5c71229928faa58823
Salim Djerbouh <caddydz4@gmail.com>
2019-07-30 16:01:50 +0100

    removed two locations
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/views/contact/locations.blade.php
+++ resources/views/contact/locations.blade.php
@@ -4,6 +4,10 @@
             <div class="row">
                 <!-- Start contact icon column -->
                 <div class="col-md-4 col-sm-4 col-xs-12">
+                    {{-- Needed empty --}}
+                </div>
+                <!-- Start contact icon column -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
                     <div class="contact-icon">
                         <div class="single-contact">
                             <h5>Siége Social</h5>
@@ -24,43 +28,7 @@
                 </div>
                 <!-- Start contact icon column -->
                 <div class="col-md-4 col-sm-4 col-xs-12">
-                    <div class="contact-icon">
-                        <div class="single-contact">
-                            <h5>Bureau D'étude</h5>
-                            <a href="{{ setting('office_address') }}">
-                                <i class="fa fa-map"></i>
-                                <span>Cité Bougara 2 H3XQ+QW Sidi Moussa</span>
-                            </a>
-                            <a href="tel:{{ setting('phone_number') }}">
-                                <i class="fa fa-phone"></i>
-                                <span>{{ setting('phone_number') }}</span>
-                            </a>
-                            <a href="mailto:{{ setting('info_email') }}">
-                                <i class="fa fa-envelope"></i>
-                                <span>{{ setting('info_email') }}</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Start contact icon column -->
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <div class="contact-icon">
-                        <div class="single-contact">
-                            <h5>Carriére</h5>
-                            <a href="{{ setting('address') }}">
-                                <i class="fa fa-map"></i>
-                                <span>L'address de carrier</span>
-                            </a>
-                            <a href="tel:{{ setting('phone_number') }}">
-                                <i class="fa fa-phone"></i>
-                                <span>{{ setting('phone_number') }}</span>
-                            </a>
-                            <a href="mailto:{{ setting('info_email') }}">
-                                <i class="fa fa-envelope"></i>
-                                <span>{{ setting('info_email') }}</span>
-                            </a>
-                        </div>
-                    </div>
+                    {{-- Needed Empty --}}
                 </div>
             </div>
         </div>
