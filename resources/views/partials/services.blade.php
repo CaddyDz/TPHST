a5320434151e0ed2d6fe82e7352fda9de6f760f9
Salim Djerbouh <caddydz4@gmail.com>
2019-08-08 15:06:40 +0100

    same height columns
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -9,24 +9,24 @@
             </div>
         </div>
         <div class="row">
-            <div class="services-all">
+            <div class="services-all equal-columns-height">
                 {{-- Start service --}}
                 @foreach ($services as $service)
                 <div class="col-md-4 col-sm-6 col-xs-12">
-                        <div class="single-services">
-                            <a class="service-images" href="{{ route('service', ['service' => $service]) }}">
-                                <i class="flaticon-{{ $service->icon }}"></i>
-                            </a>
-                            <div class="service-content">
-                                <h4>
-                                    <a href="{{ route('service', ['service' => $service]) }}">
-                                        {{ $service->name }}
-                                    </a>
-                                </h4>
-                                <p>{{ $service->description }}</p>
-                            </div>
+                    <div class="single-services">
+                        <a class="service-images" href="{{ route('service', ['service' => $service]) }}">
+                            <i class="flaticon-{{ $service->icon }}"></i>
+                        </a>
+                        <div class="service-content">
+                            <h4>
+                                <a href="{{ route('service', ['service' => $service]) }}">
+                                    {{ $service->name }}
+                                </a>
+                            </h4>
+                            <p>{{ $service->description }}</p>
                         </div>
                     </div>
+                </div>
                 @endforeach
             </div>
         </div>
--- resources/views/services/services_showcase.blade.php
+++ resources/views/services/services_showcase.blade.php
@@ -3,31 +3,27 @@
 <div class="welcome-area area-padding">
     <div class="container">
         <div class="row">
-            <div class="well-main-services">
-                @foreach ($services->take(6)->chunk(3) as $services)
-                    <div class="row">
-                        @foreach ($services as $service)
-                            <div class="col-md-4 col-sm-4 col-xs-12">
-                                <div class="well-services">
-                                    <div class="services-img">
-                                        <img src="{{ secure_asset($service->thumb) }}" alt="{{ $service->name }}">
-                                    </div>
-                                    <div class="main-services">
-                                        <div class="service-content">
-                                            <h4>
-                                                <a href="{{ route('service', ['service' => $service]) }}">
-                                                    {{ $service->name }}
-                                                </a>
-                                            </h4>
-                                            <p>{{ $service->description }}</p>
-                                            <a class="service-btn" href="{{ route('service', ['service' => $service]) }}">
-                                                @lang('read more')
-                                            </a>
-                                        </div>
-                                    </div>
+            <div class="well-main-services equal-columns-height">
+                @foreach ($services->take(6) as $service)
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div class="well-services">
+                            <div class="services-img">
+                                <img src="{{ secure_asset($service->thumb) }}" alt="{{ $service->name }}">
+                            </div>
+                            <div class="main-services">
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="{{ route('service', ['service' => $service]) }}">
+                                            {{ $service->name }}
+                                        </a>
+                                    </h4>
+                                    <p>{{ $service->description }}</p>
+                                    <a class="service-btn" href="{{ route('service', ['service' => $service]) }}">
+                                        @lang('read more')
+                                    </a>
                                 </div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 {{-- single-well end --}}
                 @endforeach
