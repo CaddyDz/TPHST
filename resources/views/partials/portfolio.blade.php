e7d9682587e63672fd59dfdd751d99e08f315ad5
Salim Djerbouh <caddydz4@gmail.com>
2019-12-22 14:46:04 +0100

    fixed project area background image symlink
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -1,24 +1,26 @@
 @if($projects->isNotEmpty())
-<div class="project-area area-padding" style="background: url('{{ secure_asset(image('Stats Background')) }}')">
+<div class="project-area area-padding"
+    style="background: url('{{ secure_asset('storage/'.image('Stats Background')) }}')">
     <div class="container">
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="section-headline white-head text-center">
                     <h3>@lang('Nos Projets')</h3>
                     <p>
-                    @lang("Le TPHST est un groupe d'ingénierie et d'entrepreneur général qui a acquis son expertise au cours des treize dernières années.")
+                        @lang("Le TPHST est un groupe d'ingénierie et d'entrepreneur général qui a acquis son expertise
+                        au cours des treize dernières années.")
                     </p>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="project-all">          
+            <div class="project-all">
                 @foreach ($projects as $project)
                 <div class="project-single all-padding">
                     <div class="single-awesome-project">
                         <div class="awesome-img">
                             <a class="venobox" data-gall="myGallery" href="{{ $project->thumb }}">
-                                <img src="{{ $project->thumb }}" alt="{{ $project->title }}" height="400"/>
+                                <img src="{{ $project->thumb }}" alt="{{ $project->title }}" height="400" />
                             </a>
                             <div class="add-actions text-center">
                                 <div class="project-dec">
